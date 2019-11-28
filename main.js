8e56a46a6692c0f87f0d808ce7a8cbcb7e8af5e9
HPringles <harry.ingles@outlook.com>
2019-11-28 14:31:43 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+module.exports.loop = () => {
+    let spawn = Game.spawns['Spawn1']
 
+    creepsNameArr = []
+    creepsArr = []
+    if (creepsArr.length < 1) {
+        newCreepName = Array(10).map(x => Math.random.toString(36).substring(7))
+        newCreep = spawn.spawnCreep([WORK, CARRY, MOVE], newCreepName, {memory: {role: 'minion'}})
+        creepsNameArr.push(newCreepName)
+        creepsArr.push(newCreep)
+    }
+    print(creepsArr)
+    print(creepsNameArr)
+
+    console.log("yee")
+
+
+}
