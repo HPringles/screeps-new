0355602bf95b7f217e927b56e7c6ede9abbc6113
HPringles <harry.ingles@outlook.com>
2019-11-28 15:02:50 +0000

    test

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const roleMinion = require('role.minion')
 
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+
 module.exports.loop = () => {
     let spawn = Game.spawns['Spawn1']
 
@@ -15,7 +26,7 @@ module.exports.loop = () => {
 
 
     if (creepsArr.length < 1) {
-        newCreepName = Array(10).map(x => Math.random.toString(36))
+        newCreepName = create_UUID()
         newCreep = spawn.spawnCreep([WORK, CARRY, MOVE], newCreepName, {memory: {role: 'minion'}})
         if (newCreep >= 0) {
             creepsNameArr.push(newCreepName)
